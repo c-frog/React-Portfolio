4bff69a50e2fb78146b089b61935a8b192b0ee07
Clyde Plasencia <cquiatt@gmail.com>
2020-02-21 16:22:30 -0700

    update to w&e component

--- src/components/w&e/w&e.js
+++ src/components/w&e/w&e.js
@@ -8,7 +8,12 @@ import WeddingParty from '../../assets/images/w&e/weddingPary.jpg';
 import Alter from '../../assets/images/w&e/jeAlter.jpg';
 import ExitAisle from '../../assets/images/w&e/jeExit.jpg';
 
-// import ElizaAisle from '../../assets/images/w&e/elizaAisle.jpg';
+import Bridesmaids from '../../assets/images/w&e/K-G-Welcome-Party-104.jpg';
+import FamilyShot from '../../assets/images/w&e/K-G-Welcome-Party-29.jpg';
+import Tent from '../../assets/images/w&e/K-G-Welcome-Party-66.jpg';
+import KatieAndDad from '../../assets/images/w&e/K-G-Welcome-Party-25 copy.jpg';
+import GarrettToast from '../../assets/images/w&e/K-G-Welcome-Party-52.jpg';
+import KatieAndGarrett from '../../assets/images/w&e/K-G-Welcome-Party-60.jpg';
 
 function Weddings () {
    return (
@@ -19,6 +24,7 @@ function Weddings () {
             <div className="r1">
             
                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+
                   <div className="carousel-inner">
                      <div className="carousel-item active">
                         <img src={ElizaAisle} className="d-block w-100" alt="..." />
@@ -30,6 +36,7 @@ function Weddings () {
                         <img src={Groomsmen} className="d-block w-100" alt="..."/>
                      </div>
                   </div>
+
                   <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                      <span className="sr-only">Previous</span>
@@ -38,12 +45,12 @@ function Weddings () {
                      <span className="carousel-control-next-icon" aria-hidden="true"></span>
                      <span className="sr-only">Next</span>
                   </a>
+
                </div>
             
                <div>
                   <img className="soloStill" src={Gazebo} alt=""/>
                </div>
-            
                
             </div>
             
@@ -58,9 +65,46 @@ function Weddings () {
 
 
 
+            <div className="r1">
+
+               <div id="carouselExampleIndicators1" className="carousel slide" data-ride="carousel">
+
+                  <div className="carousel-inner">
+                     <div className="carousel-item active">
+                        <img src={Bridesmaids} className="d-block w-100" alt="..." />>
+                     </div>
+                     <div className="carousel-item">
+                        <img src={FamilyShot} className="d-block w-100" alt="..." />
+                     </div>
+                     <div className="carousel-item">
+                        <img src={Tent} className="d-block w-100" alt="..." />
+                     </div>
+                  </div>
+
+                  <a className="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">
+                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                     <span className="sr-only">Previous</span>
+                  </a>
+                  <a className="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next">
+                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                     <span className="sr-only">Next</span>
+                  </a>
+
+               </div>
+
+               <div>
+                  <img src={KatieAndDad} alt="" className="soloStill" />
+               </div>
+
+            </div>
+
+               <div className="stills">
+                  <img src={GarrettToast} alt="" className="still" />
+                  <img src={KatieAndGarrett} className="still" alt="" />
+                  <span className="stretch"></span>
+               </div>
 
       </div>
-
    )
 }
 
